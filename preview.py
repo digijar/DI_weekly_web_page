64316f64c4812682fc7110d5f2929fb0e6a61965
JARON CHAN <jaronchan123@gmail.com>
2023-09-21 10:04:21 +0800

    updated with "Add Row" function

--- preview.py
+++ preview.py
@@ -11,6 +11,7 @@ from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.styles import Font
 import datetime
+from datetime import date
 
 app = FastAPI()
 
@@ -250,6 +251,79 @@ async def update_mergermarket(request: Request):
         success = False
     
     return {"success": success}
+
+class MergerMarketRow(BaseModel):
+    Opportunity_ID: float
+    Date: date
+    Value_INR_m: str
+    Value_Description: str
+    Heading: str
+    Opportunity: str
+    Targets: str
+    Lead_Type: str
+    Type_of_transaction: str
+    HS_sector_classification: str
+    Short_BD: str
+    Source: str
+    Intelligence_Type: str
+    Intelligence_Grade: str
+    Intelligence_Size: str
+    Stake_Value: str
+    Dominant_Sector: str
+    Sectors: str
+    Sub_Sectors: str
+    Dominant_Geography: str
+    Geography: str
+    States: str
+    Topics: str
+    Bidders: str
+    Vendors: str
+    Issuers: str
+    Competitors: str
+    Others: str
+    Completed: str
+
+@app.post('/add_mergermarket_row')
+async def add_mergermarket_row(request: Request, row_data: MergerMarketRow):
+    table_name = "MergerMarket"
+
+    try:
+        # Initialize the BigQuery client
+        client = bigquery.Client()
+
+        # Convert Date to ISO formatted string
+        formatted_date = row_data.Date.isoformat()
+
+        # Construct the INSERT query dynamically
+        insert_query = f"""
+        INSERT INTO `testing-bigquery-vertexai.templates.{table_name}`
+        (Opportunity_ID, Date, `Value INR_m`, `Value Description`, Heading, Opportunity, Targets,
+        `Lead type`, `Type of transaction`, `HS sector classification`, `Short BD`, Source,
+        `Intelligence Type`, `Intelligence Grade`, `Intelligence Size`, `Stake Value`, `Dominant Sector`,
+        Sectors, `Sub Sectors`, `Dominant Geography`, Geography, States, Topics, Bidders, Vendors,
+        Issuers, Competitors, Others, Completed)
+        VALUES 
+        ({row_data.Opportunity_ID}, '{formatted_date}', '{row_data.Value_INR_m}', '{row_data.Value_Description}',
+        '{row_data.Heading}', '{row_data.Opportunity}', '{row_data.Targets}', '{row_data.Lead_Type}',
+        '{row_data.Type_of_transaction}', '{row_data.HS_sector_classification}', '{row_data.Short_BD}',
+        '{row_data.Source}', '{row_data.Intelligence_Type}', '{row_data.Intelligence_Grade}',
+        '{row_data.Intelligence_Size}', '{row_data.Stake_Value}', '{row_data.Dominant_Sector}',
+        '{row_data.Sectors}', '{row_data.Sub_Sectors}', '{row_data.Dominant_Geography}', '{row_data.Geography}',
+        '{row_data.States}', '{row_data.Topics}', '{row_data.Bidders}', '{row_data.Vendors}', '{row_data.Issuers}',
+        '{row_data.Competitors}', '{row_data.Others}', '{row_data.Completed}')
+        """
+
+        # Run the query
+        query_job = client.query(insert_query)
+
+        # Wait for the query to complete (optional)
+        query_job.result()
+
+        return {"success": True}
+
+    except Exception as e:
+        print(e)
+        return {"success": False}
 
 @app.get("/download_marketscan")
 def download_MS():
@@ -525,6 +599,87 @@ async def update_marketscan(request: Request):
     
     return {"success": success}
 
+class MarketScanRow(BaseModel):
+    Num: float
+    Picks: bool
+    Captured_in_the_week: str
+    Date: float
+    Lead_type: str
+    Target_HS_industry_classification: str
+    Dominant_sector_as_per_MM: str
+    Sectors_as_per_MM: str
+    Sub_Sectors_as_per_MM: str
+    HS_Target_region: str
+    Dominant_country: str
+    Countries: str
+    Next_Steps: str
+    Target: str
+    Target_Chinese: str
+    Deal_intelligence: str
+    Short_BD: str
+    Bidders: str
+    Sellers_or_Vendors: str
+    Type_of_transaction: str
+    Intelligence_type_as_per_MM: str
+    Topics_as_per_MM: str
+    Value_USDm: str
+    Value_description: str
+    Intelligence_size: str
+    Intelligence_size_bucket: str
+    Stake_value_percent: str
+    Held_by_ASPAC_priority_firm_Y_or_N: str
+    PE_priority_firm: str
+    Held_since: str
+    Held_for_more_than_three_years_Y_N_NA: str
+    KPMG_credentials_PE_Company_Both_N: str
+    KPMG_firm: str
+    Engagement_partner: str
+
+@app.post('/add_marketscan_row')
+async def add_marketscan_row(request: Request, row_data: MarketScanRow):
+    table_name = "Market_Scan"
+
+    try:
+        # Initialize the BigQuery client
+        client = bigquery.Client()
+
+        # Construct the INSERT query dynamically
+        insert_query = f"""
+        INSERT INTO `testing-bigquery-vertexai.templates.{table_name}`
+        (Num, Picks, `Captured in the week`, Date, `Lead type`, `Target HS industry classification`,
+        `Dominant sector_as per MM`, `Sectors_as per MM`, `Sub Sectors_as per MM`, `HS Target region`,
+        `Dominant country`, Countries, `Next Steps`, Target, Target_Chinese, `Deal intelligence`,
+        `Short BD`, Bidders, Sellers_or_Vendors, `Type of transaction`, `Intelligence type_as per MM`,
+        `Topics_as per MM`, `Value_USDm`, `Value description`, `Intelligence size`, `Intelligence size bucket`,
+        `Stake value_percent`, `Held by ASPAC priority firm_Y_or_N`, `PE priority firm `, `Held since`,
+        `Held for more than three years_Y_N_NA`, `KPMG credentials_PE_Company_Both_N`, `KPMG firm`,
+        `Engagement partner`)
+        VALUES 
+        ({row_data.Num}, {row_data.Picks}, '{row_data.Captured_in_the_week}', {row_data.Date}, 
+        '{row_data.Lead_type}', '{row_data.Target_HS_industry_classification}', '{row_data.Dominant_sector_as_per_MM}',
+        '{row_data.Sectors_as_per_MM}', '{row_data.Sub_Sectors_as_per_MM}', '{row_data.HS_Target_region}',
+        '{row_data.Dominant_country}', '{row_data.Countries}', '{row_data.Next_Steps}', '{row_data.Target}',
+        '{row_data.Target_Chinese}', '{row_data.Deal_intelligence}', '{row_data.Short_BD}', '{row_data.Bidders}',
+        '{row_data.Sellers_or_Vendors}', '{row_data.Type_of_transaction}', '{row_data.Intelligence_type_as_per_MM}',
+        '{row_data.Topics_as_per_MM}', '{row_data.Value_USDm}', '{row_data.Value_description}', 
+        '{row_data.Intelligence_size}', '{row_data.Intelligence_size_bucket}', '{row_data.Stake_value_percent}',
+        '{row_data.Held_by_ASPAC_priority_firm_Y_or_N}', '{row_data.PE_priority_firm}', '{row_data.Held_since}',
+        '{row_data.Held_for_more_than_three_years_Y_N_NA}', '{row_data.KPMG_credentials_PE_Company_Both_N}',
+        '{row_data.KPMG_firm}', '{row_data.Engagement_partner}')
+        """
+
+        # Run the query
+        query_job = client.query(insert_query)
+
+        # Wait for the query to complete (optional)
+        query_job.result()
+
+        return {"success": True}
+
+    except Exception as e:
+        print(e)
+        return {"success": False}
+
 
 @app.get("/download_rollingshortlist")
 def download_RS():
@@ -785,6 +940,73 @@ async def update_rollingshortlist(request: Request):
     
     return {"success": success}
 
+class RollingShortlistRow(BaseModel):
+    Num: float
+    Captured_date: float
+    BP_comments: str
+    Partner_or_Director: str
+    Target_country: str
+    Sub_sector: str
+    Source: str
+    Target: str
+    Business_Description: str
+    Financials: str
+    Revenue_USD_M: str
+    EBITDA_USD_M: str
+    Valuation_USD_M: str
+    Other_Info: str
+    Deal_Intelligence_info: str
+    News_Date: float
+    KPMG_View_Redacted: str
+    Credentials: str
+    HS_contact: str
+    Investment_date: str
+    Geographic_region: str
+    Asset_pack: str
+    Target_Region: str
+    Shareholders: str
+    Lead_type: str
+    Target_HS_industry_classification: str
+    Stake_for_sale_percent: str
+    Value_USD_M: str
+    Value_description: str
+    Website: str
+
+@app.post('/add_rollingshortlist_row')
+async def add_rollingshortlist_row(request: Request, row_data: RollingShortlistRow):
+    table_name = "Rolling_Shortlist"
+
+    try:
+        # Initialize the BigQuery client
+        client = bigquery.Client()
+
+        # Construct the INSERT query dynamically
+        insert_query = f"""
+        INSERT INTO `testing-bigquery-vertexai.templates.{table_name}`
+        (Num, `Captured date`, `BP comments`, `Partner_or_Director`, `Target country`, `Sub sector`, `Source`, ` Target`, `Business Description`, `Financials`, `Revenue_USD_M`, `EBITDA_USD_M`, `Valuation_USD_M`, `Other Info`, `Deal Intelligence info`, `News Date`, `KPMG View - Redacted`, `Credentials`, `HS contact`, `Investment date`, `Geographic region`, `Asset pack `, `Target Region`, `Shareholders`, `Lead type`, `Target HS industry classification `, `Stake for sale_percent`, `Value_USD_M`, `Value description `, `Website`) VALUES 
+        ({row_data.Num}, {row_data.Captured_date}, '{row_data.BP_comments}', '{row_data.Partner_or_Director}', 
+        '{row_data.Target_country}', '{row_data.Sub_sector}', '{row_data.Source}', '{row_data.Target}', 
+        '{row_data.Business_Description}', '{row_data.Financials}', '{row_data.Revenue_USD_M}', 
+        '{row_data.EBITDA_USD_M}', '{row_data.Valuation_USD_M}', '{row_data.Other_Info}', 
+        '{row_data.Deal_Intelligence_info}', {row_data.News_Date}, '{row_data.KPMG_View_Redacted}', 
+        '{row_data.Credentials}', '{row_data.HS_contact}', '{row_data.Investment_date}', 
+        '{row_data.Geographic_region}', '{row_data.Asset_pack}', '{row_data.Target_Region}', 
+        '{row_data.Shareholders}', '{row_data.Lead_type}', '{row_data.Target_HS_industry_classification}', 
+        '{row_data.Stake_for_sale_percent}', '{row_data.Value_USD_M}', '{row_data.Value_description}', 
+        '{row_data.Website}')
+        """
+
+        # Run the query
+        query_job = client.query(insert_query)
+
+        # Wait for the query to complete (optional)
+        query_job.result()
+
+        return {"success": True}
+
+    except Exception as e:
+        print(e)
+        return {"success": False}
 
 if __name__ == '__main__':
     import uvicorn
